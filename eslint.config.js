2bdd687e9533b2b65ebf4bbee6132146b0d5cf5c
Patrick Hadson <pkhadson@gmail.com>
2025-10-06 19:40:54 -0300

    Refactor ESLint configuration: replace TypeScript config file with JavaScript version, update ESLint and related dependencies to version 9.37.0, and enhance global settings for Node.js compatibility.

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  { languageOptions: { globals: { ...globals.node } } },
+];
+
